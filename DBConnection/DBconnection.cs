a3e42995ef19cb165b9c12a31dae9c4d048a3d4a
unknown <Matthew@.(none)>
2011-08-05 21:27:34 +0800

    * Code cleanup, who taught you how to clean up your code! Probably the same person who taught me :)

--- DBConnection/DBconnection.cs
+++ DBConnection/DBconnection.cs
@@ -13,113 +13,113 @@ using System.IO;
 
 namespace EveControlPanelApplication
 {
-   
+
     class DBConnect
     {
-    private MySqlConnection connection;
-    private string server;
-    private string database;
-    private string uid;
-    private string password;
-    private EveControlPanelApplication.mySqlLogin DBinfo = new EveControlPanelApplication.mySqlLogin(); 
-        
-    //Constructor
-   public DBConnect()
-  {
-       Initialize();
-       
-   }
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private EveControlPanelApplication.mySqlLogin DBinfo = new EveControlPanelApplication.mySqlLogin();
 
-    //Initialize values
-    private void Initialize()
-    {
-
-
-
-        server = DBinfo.hostTextBox.Text;    
-        database = DBinfo.databaseTextBox.Text;
-        uid = DBinfo.usernameTextBox.Text;
-        password = DBinfo.passwordTextBox.Text;
-        string connectionString;
-        connectionString = "SERVER=" + server + ";" + "DATABASE=" + 
-		database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-
-        connection = new MySqlConnection(connectionString);
-    }
-
-    //open connection to database
-    private bool OpenConnection()
-    {
-        try
+        //Constructor
+        public DBConnect()
         {
-            connection.Open();
-            return true;
+            Initialize();
         }
-        catch (MySqlException ex)
-        { //When handling errors, you can your application's response based 
-        //on the error number.
-        //The two most common error numbers when connecting are as follows:
-        //0: Cannot connect to server.
-        //1045: Invalid user name and/or password.
-        switch (ex.Number)
-        {
-            case 0:
-                MessageBox.Show("Cannot connect to server.  Contact administrator");
-                break;
 
-            case 1045:
-                MessageBox.Show("Invalid username/password, please try again");
-                break;
+        //Initialize values
+        private void Initialize()
+        {
+            // We need a cleaner way of doing this, i think accessing the settings we already have through the xml file.
+            server = DBinfo.hostTextBox.Text;
+            database = DBinfo.databaseTextBox.Text;
+            uid = DBinfo.usernameTextBox.Text;
+            password = DBinfo.passwordTextBox.Text;
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
         }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            { 
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
                 return false;
-        
+
+            }
         }
-    }
 
-    //Close connection
-    private bool CloseConnection()
-    {
-        try
-                {
-                    connection.Close();
-                    return true;
-                }
-         catch (MySqlException ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    return false;
-                }
-    }
-
-// Example of what an insert should look like
-    public void Insert()
-    {
-        string query = "INSERT INTO tabelinfo (age) VALUES('32')";
-
-        //open connection
-        if (this.OpenConnection() == true)
+        //Close connection
+        private bool CloseConnection()
         {
-            //create command and assign the query and connection from the constructor
-            MySqlCommand cmd = new MySqlCommand(query, connection);
-
-            //Execute command
-            cmd.ExecuteNonQuery();
-
-            //close connection
-            this.CloseConnection();
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
         }
-    }
-   
 
-    //Backup
-    public void Backup()
-    {
-    }
+        // Example of what an insert should look like
+        public void Insert()
+        {
+            string query = "INSERT INTO tabelinfo (age) VALUES('32')";
 
-    //Restore
-    public void Restore()
-    {
-    }
-    
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+
+        //Backup
+        public void Backup()
+        {
+
+        }
+
+        //Restore
+        public void Restore()
+        {
+
+        }
+
     }
 }
